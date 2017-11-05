dd6ad1b808df2d26e946a1bb07b215b14bfa1deb
Sagang Wee <sagangwee@yahoo.com>
2017-11-04 19:26:23 -0700

    Integrated Stellar Template

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -7,10 +7,22 @@ exports.getTwitter = (req, res, next) => {
     access_token: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   });
-  T.get('search/tweets', { q: 'nodejs since:2013-01-01', geocode: '40.71448,-74.00598,5mi', count: 10 }, (err, reply) => {
+  let lastMonthDate = new Date();
+  lastMonthDate.setDate(1);
+  lastMonthDate.setMonth(lastMonthDate.getMonth()-1);
+  const lastMonthISODate = lastMonthDate.toISOString().slice(0, 10)
+  const query = { q: 'I wish there was an easier way since:' + lastMonthISODate };
+  T.get('search/tweets', query , (err, reply) => {
     if (err) {
       console.log(err);
     }
-    res.json(reply)
+    const topFiveTweets = getTopFiveTweets(reply.statuses);
+    res.json(topFiveTweets);
   });
 };
+
+const getTopFiveTweets = tweets => {
+  const originalTweets = tweets.filter(tweet => !tweet.retweeted);
+  let sortedOriginalTweets = originalTweets.sort((a, b) => b.favorite_count - a.favorite_count);
+  return sortedOriginalTweets.slice(0, 5);
+}
