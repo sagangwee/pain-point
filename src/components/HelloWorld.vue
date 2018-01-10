3ef84178f785c25ab1b705d85858829a608a2342
Sagang Wee <sagangwee@yahoo.com>
2018-01-10 02:54:39 -0800

    Removed comments

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,7 +34,7 @@
                     these complaints to generate solutions?
                   </p>
                   <ul class="actions">
-                    <li><a href="generic.html" class="button">Learn More</a></li>
+                    <li><a href="#first" class="button">Let's Do It</a></li>
                   </ul>
                 </div>
                 <span class="image"><img src="~@/assets/images/pic01.jpg" alt="" /></span>
@@ -62,11 +62,6 @@
                 </li>
               </ul>
               <button class="special" v-on:click="getTopFiveTweets">Get Top Five Tweets</button>
-              <!-- <footer class="major">
-                <ul class="actions">
-                  <li><a href="generic.html" class="button">Learn More</a></li>
-                </ul>
-              </footer> -->
             </section>
 
           <!-- Second Section -->
@@ -81,34 +76,6 @@
                   <!-- <button v-on:click="onSubmit(tweet._id)">Delete Tweet</button> -->
                 </li>
               </ul>
-              <!-- <ul class="statistics">
-                <li class="style1">
-                  <span class="icon fa-code-fork"></span>
-                  <strong>5,120</strong> Etiam
-                </li>
-                <li class="style2">
-                  <span class="icon fa-folder-open-o"></span>
-                  <strong>8,192</strong> Magna
-                </li>
-                <li class="style3">
-                  <span class="icon fa-signal"></span>
-                  <strong>2,048</strong> Tempus
-                </li>
-                <li class="style4">
-                  <span class="icon fa-laptop"></span>
-                  <strong>4,096</strong> Aliquam
-                </li>
-                <li class="style5">
-                  <span class="icon fa-diamond"></span>
-                  <strong>1,024</strong> Nullam
-                </li>
-              </ul>
-              <p class="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p>
-              <footer class="major">
-                <ul class="actions">
-                  <li><a href="generic.html" class="button">Learn More</a></li>
-                </ul>
-              </footer> -->
             </section>
 
           <!-- Get Started -->
